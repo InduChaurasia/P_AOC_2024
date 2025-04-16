cff22e7ec06d6db9e17d9e57baba6a3241d4e9c2
InduChaurasia <indu.bitmesra@gmail.com>
2025-04-16 00:50:21 -0500

    AOC day 25 , 2024

---
+++ src/__2024__/day25.py
@@ -0,0 +1,42 @@
+"""AOC day 25: https://adventofcode.com/2024/day/25"""
+
+from utilities import read_file,Year
+
+def read_input():
+    locks, keys = [], []
+    current, is_lock = [], True
+
+    for line in read_file(Year.y2024, 'day25.txt'):
+        if not line:
+            if current:
+                (locks if is_lock else keys).append(current)
+                current = []
+            continue
+        if not current:
+            is_lock = '.' not in line
+        current.append(list(line))
+
+    if current:
+        (locks if is_lock else keys).append(current)
+
+    return locks, keys
+
+def do_fit(lock,key):
+    rows= len(lock)
+    cols= len(lock[0])
+    for i in range(rows):
+        for j in range(cols):
+            if lock[i][j] == '#' and key[i][j] == '#':
+                return False
+    return True
+
+def find_fitting_keys():
+    locks,keys=read_input()
+    fit_count=sum( 1 for lock in locks for key in keys if do_fit(lock,key))
+    print(fit_count)
+
+def main():
+    find_fitting_keys()
+
+if __name__ == '__main__':
+    main()
