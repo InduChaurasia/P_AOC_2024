87868ed153d1c53b96e6f93fce7a9f1df3ced9a5
InduChaurasia <indu.bitmesra@gmail.com>
2025-12-05 12:13:25 -0600

    AOC 2025 day5

---
+++ src/__2025__/day5.py
@@ -0,0 +1,57 @@
+'''https://adventofcode.com/2025/day/5'''
+from utilities import read_file,Year
+
+def read_input():
+    ranges = []
+    product_ids=[]
+
+    for line in  read_file(Year.y2025, 'day5.txt'):
+        if not line:
+            continue
+        if '-' in line:
+            start,end = line.split('-')
+            ranges.append( (int(start), int(end)) )
+        else:
+            product_ids.append(int(line))
+        ranges.sort()
+    return ranges, product_ids
+
+def find_product_id(ranges,product_id,s,e)->str:
+    if e in (s,s+1):
+        if ranges[s][0]<=product_id<=ranges[s][1]:
+            return True
+        if ranges[e][0]<=product_id<=ranges[e][1]:
+            return True
+        return False
+    mid = (s+e)//2
+    return find_product_id(ranges,product_id,s,mid) or find_product_id(ranges,product_id,mid+1,e)
+
+def fresh_inventrory(ranges,product_ids)->int:
+    fresh= 0
+    for product_id in product_ids:
+        if find_product_id(ranges,product_id,0,len(ranges)-1):
+            fresh+=1
+    return fresh
+
+def fresh_product_ids(ranges)->int:
+    merged_ranges=[ranges[0]]
+    for i,(s,e) in enumerate(ranges):
+        if i==0:
+            continue
+        last_s,last_e = merged_ranges[-1]
+        if s<=last_e:
+            merged_ranges[-1]=(last_s,max(e,last_e))
+        else:
+            merged_ranges.append( (s,e) )
+    product_id_count=0
+    for s,e in merged_ranges:
+        product_id_count+=e - s + 1
+    return product_id_count
+
+def main():
+    ranges,product_ids = read_input()
+    print(f'fresh inventry: {fresh_inventrory(ranges,product_ids)}')
+    print(f'fresh product ids: {fresh_product_ids(ranges)}')
+
+if __name__ == '__main__':
+    main()
